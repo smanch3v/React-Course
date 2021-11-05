68acda2be0dab1ce2e3536ef3011a98e8c61d0f9
smanch3v <svetoslavmanchev@gmail.com>
2021-11-05 14:50:48 +0100

    first commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+  const clickHandler =() => {
+    console.log('Clicked!!!!');
+  }
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
